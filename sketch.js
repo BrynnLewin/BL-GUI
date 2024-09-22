2ee684fd9dfb5d31160f848657cab4026225a28d
Brynn Lewin <bhl21@fsu.edu>
2024-09-22 17:28:36 -0400

    joystick working

--- sketch.js
+++ sketch.js
@@ -7,8 +7,7 @@ let gui;
 let diameterSlider, degSlider, outlineCheckbox;
 
 let joystick;
-let circleColor;
-let rectColor;
+let bgColor;
 
 function setup() {
   createCanvas(400, 400);
@@ -20,44 +19,51 @@ function setup() {
 
   joystick = createJoystick("Joystick", 30, 10, 60,60);
   joystick.setStyle({
-    fillBg: color("red")
+    fillBg: color("white")
   })
+  bgColor = color("red");
 
-  circleColor = color("red");
-  rectColor = color("red");
 
   diameterSlider = createSlider("Diameter", 200,10, 100,20, 10,100);
   diameterSlider.onChange = diameterSliderChanger;
   diameterSlider.setStyle({
-    fillBg: color("red"),
+    fillBg: color("white"),
     strokeWeight: 5
   })
   degSlider = createSlider("Rotation", 200,40, 100,20, 0,180);
   degSlider.setStyle({
-    fillBg: color("red"),
+    fillBg: color("white"),
     strokeWeight: 5
   })
   degSlider.isInteger = true;
   degSlider.val = 23;
   outlineCheckbox = createCheckbox("Outline", 340,10, 20,20, true);
   outlineCheckbox.setStyle({
-    fillBg: color("red"),
+    fillBg: color("white"),
     strokeWeight: 2
   })
 }
 
 function draw() {
-  background("black");
+  let joyX = joystick.valX;
+  let joyY = joystick.valY;
+
+  let r = map(joyX, -1, 1, 0, 255); 
+  let g = map(joyY, -1, 1, 0, 255); 
+  let b = map(joyX + joyY, -2, 2, 0, 255)
+
+  bgColor = color(r,g,b);
+
+  background(bgColor);
   drawGui();
 
   if (outlineCheckbox.val) {
-  stroke("grey");
+  stroke("black");
   } else {
     noStroke();
   }
 
-
-  fill (color("red"));
+Fill: color("white");
   circle(100,200,d);
 
 
